aaa8b0fcdf2d7132f60ac057efaf2201bfd69734
zawadzamil <contact.zawadzamil@gmail.com>
2023-08-29 18:30:03 +0600

    added cookie

--- app/api/route.js
+++ app/api/route.js
@@ -1,10 +1,22 @@
 import { NextResponse } from "next/server";
 
-export async function POST(req, res){
+export async function POST(req, res) {
+    const jsonBody = await req.json();
+
+    let lang = jsonBody["language"];
+    let theme = jsonBody["theme"];
+
     return NextResponse.json(
         {
-            status:200,
-            message: "Request Completed!"
+            message: "Cookie Saved!",
+            lang: lang,
+            theme: theme,
+        },
+        {
+            status: 200,
+            headers: {
+                "Set-Cookie": `lang = ${lang}, theme = ${theme}, path = /`,
+            },
         }
-    )
+    );
 }
--- app/page.js
+++ app/page.js
@@ -1,22 +1,29 @@
-'use client';
+"use client";
 export default async function Home() {
-  const handelClick = async () =>{
-    const response = await fetch("/api",{
-      method: "POST",
-      body: {
-        name: "Zawad"
-      }
-    });
+    const handelClick = async () => {
+        const response = await fetch("/api", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                theme: "dark",
+                language: "en",
+            }),
+        });
 
-    console.log(await response.json())
-  }
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className='w-full flex justify-center'>
-    <button className="bg-red-600 p-4 rounded-md text-white" onClick={handelClick}>
-      Set Cookie
-    </button>
-      </div>
-    </main>
-  )
+        console.log(await response.json());
+    };
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="w-full flex justify-center">
+                <button
+                    className="bg-red-600 p-4 rounded-md text-white"
+                    onClick={handelClick}
+                >
+                    Set Cookie
+                </button>
+            </div>
+        </main>
+    );
 }
